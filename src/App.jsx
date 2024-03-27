af369ce530e7987965385dc17bc42d17790d3be7
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2024-03-28 02:02:49 +0600

    magic memory project setup and button and images setup done

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+import SingleCard from './components/SingleCard/SingleCard'
+
+const cardImages = [
+  { src: "../public/img/helmet-1.png" },
+  { src: "../public/img/potion-1.png" },
+  { src: "../public/img/ring-1.png" },
+  { src: "../public/img/scroll-1.png" },
+  { src: "../public/img/shield-1.png" },
+  { src: "../public/img/sword-1.png" },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffleCards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, 'id': Math.random()}));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
+  return (
+    <>
+      <div className="App">
+        <h1>Magic Match</h1>
+        <button onClick={shuffleCards}>New Game</button>
+
+        <div className="card-grid">
+        {
+          cards.map((card) => (
+            <SingleCard key={card.id} card={card}/>
+          ))
+        }
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
