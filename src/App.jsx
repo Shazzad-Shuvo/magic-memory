1a1d40bf6d24d0f134bf097a75cf378485ce728c
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2024-03-28 02:51:29 +0600

    event handler function added for card matching

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import SingleCard from './components/SingleCard/SingleCard'
+import SingleCard from "./components/SingleCard/SingleCard";
 
 const cardImages = [
   { src: "../public/img/helmet-1.png" },
@@ -14,19 +14,43 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffleCards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({...card, 'id': Math.random()}));
+      .map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffledCards);
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  // handle Choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
+  // compare choices
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("Images matched");
+        resetChoice();
+      } else {
+        console.log("Images did not match");
+        resetChoice();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choice and increase turn
+  const resetChoice = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurn) => (prevTurn += 1));
+  };
   return (
     <>
       <div className="App">
@@ -34,11 +58,13 @@ function App() {
         <button onClick={shuffleCards}>New Game</button>
 
         <div className="card-grid">
-        {
-          cards.map((card) => (
-            <SingleCard key={card.id} card={card}/>
-          ))
-        }
+          {cards.map((card) => (
+            <SingleCard
+              key={card.id}
+              card={card}
+              handleChoice={handleChoice}
+            />
+          ))}
         </div>
       </div>
     </>
--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,14 +1,25 @@
-import './SingleCard.css'
+import "./SingleCard.css";
 
-const singleCard = ({card}) => {
+const singleCard = ({ card, handleChoice }) => {
+
+  const handleClick = () =>{
+    handleChoice(card);
+    
+  }
+
   return (
     <div>
       <div className="card">
-          <div>
-            <img className="front" src={card.src} alt="card front" />
-            <img className="back" src="/img/cover.png" alt="card back" />
-          </div>
+        <div>
+          <img className="front" src={card.src} alt="card front" />
+          <img 
+            className="back" 
+            src="/img/cover.png" 
+            alt="card back" 
+            onClick={handleClick}
+          />
         </div>
+      </div>
     </div>
   );
 };
